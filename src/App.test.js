723afe8531f30200198b2835cb88dbce798d5476
Perren Wright <34662374+perrenwright@users.noreply.github.com>
2020-03-12 17:24:47 -0700

    Update of App.test.js
    updated test component for output

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-
+import {BrowserRouter as Router} from 'react-router-dom';
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<Router><App /></Router>);
+  const linkElement = getByText(/3 Second Song Challenge/i);
   expect(linkElement).toBeInTheDocument();
 });
