0614888f0f83d6e5aec6efd2bf54562852bbe57b
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2020-03-13 15:52:40 -0700

    Set up Spotify Login (#11)
    * Set up Spotify Login
    
    * Update.package lock.json
    
    Fixed dependencies to work
    
    * Clean-Up & Testing
    
    Code clean up and writing some tests
    
    * Update Login Button
    
    tiny formating change
    
    * update with fixed yaml
    
    fixing auto labeler issue
    
    * Changed Public to Build in firebase.json
    
    Co-authored-by: Perren Wright <34662374+perrenwright@users.noreply.github.com>

---
+++ public/config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD2Cgk-IWFs4T3NdG93L5lp1bsStfb9xWA",
+  authDomain: "secondsongchallenge.firebaseapp.com",
+  databaseURL: "https://secondsongchallenge.firebaseio.com",
+  projectId: "secondsongchallenge",
+  storageBucket: "secondsongchallenge.appspot.com",
+  messagingSenderId: "578310806195",
+  appId: "1:578310806195:web:cd29ef97e3654327286ec6"
+};
+
+const spotifyConfig = {
+  clientId: '9a9b0b83d79a4ece84e608715bc5e114',
+  redirectUri: 'https://secondsongchallenge.firebaseapp.com/callback',
+};
--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import authenticate from './authenticate.js';
+import LoginButton from './components/LoginButton';
+import SpotifyWebApi from 'spotify-web-api-js';
+import getToken from './GetToken'
+import getPlaylistInfo from './getPlaylistInfo'
 
-function App() {
+export default function App() {
+  const spotifyApi = new SpotifyWebApi();
+  const token = getToken();
+  getPlaylistInfo(token)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Eit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Testing that we're here</h1>
+        <LoginButton />
       </header>
     </div>
   );
 }
-
-export default App;
