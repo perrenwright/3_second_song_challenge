0f92d69ddc47ad748d8960b0497b098ce165daaa
tonioshikanlu <toni.oshikanlu9@gmail.com>
2020-03-15 15:36:45 -0700

    Tests directory error fixed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,9 +6,3 @@ test('simple rendering test', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/3 SECOND CHALLENGE/i);
 });
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/login/i);
-  expect(linkElement).toBeInTheDocument();
-});
