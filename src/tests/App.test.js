3a339e30dd965d01af48cfda26f421172e262298
tonioshikanlu <toni.oshikanlu9@gmail.com>
2020-03-15 15:33:34 -0700

    Tests directory error fixed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('simple rendering test', () => {
   const { getByText } = render(<App />);
