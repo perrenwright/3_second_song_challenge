81dc321774cd03fac4b40f9ad34c5a92d878ef07
Perren Wright <34662374+perrenwright@users.noreply.github.com>
2020-03-19 00:49:04 -0500

    Update with test progress
    Updates the app.test files

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,6 +4,6 @@ import App from '../App';
 import {BrowserRouter as Router} from 'react-router-dom';
 test('renders learn react link', () => {
   const { getByText } = render(<Router><App /></Router>);
-  const linkElement = getByText(/3 Second Song Challenge/i);
+  const linkElement = getByText(/Challenges/i);
   expect(linkElement).toBeInTheDocument();
 });
