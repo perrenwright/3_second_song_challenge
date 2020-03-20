a9ba0bdbfafc04e06f87958f56a23a276890b4dc
Perren Wright <34662374+perrenwright@users.noreply.github.com>
2020-03-20 13:19:10 -0500

    folder creation
    organizing the components by folders

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PlayChallenge from './ChallengePage.js';
+import PlayChallenge from './components/ChallengePage.js';
 function App() {
   return (
     <div className="App">
-      {/* <div>
-        Space 
-      </div> */}
-      <div>
-        <PlayChallenge/>
-      </div>
     </div>
   );
 }
