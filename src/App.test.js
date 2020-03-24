fb7ffc91af7a4b2eeaa49ac127e339b2cdbcd214
Mikayla Orange <mikayla.orange@bison.howard.edu>
2020-03-24 19:48:31 -0400

    fixed testing issue with original commit

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-test('simple rendering test', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/3 SECOND CHALLENGE/i);
-  expect(linkElement).toBeInTheDocument();
-});
