fc8de46d0fc6de99fd2f09b6805d4186be0b670f
prabinspkt <prabin4998@gmail.com>
2020-03-29 00:09:00 -0700

    Get playlists from user's Spotify account

--- src/GetToken.js
+++ src/GetToken.js
@@ -1,20 +1,19 @@
 import SpotifyWebApi from 'spotify-web-api-js';
 
 export default function getToken() {
+  // This function grabs the user's access token from the url so we can grab the information
+  // about that user from the database.
+  var hashParams = {};
+  var e,
+    r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+  e = r.exec(q);
+  while (e) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+    e = r.exec(q);
+  }
 
-    // This function grabs the user's access token from the url so we can grab the information 
-    // about that user from the database.
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
-    }
-
-    const token = hashParams.access_token;
-    return token;
-  
+  const token = hashParams.access_token;
+  console.log('Token: ', token);
+  return token;
 }
-
--- src/components/challenges.js
+++ src/components/challenges.js
@@ -1,8 +1,75 @@
-import React from 'react';
-export default function challenges()
-{
-    console.log("we here!");
-    return(
-        <h1>challenges</h1>
-    );
+import React, { useState, useEffect } from 'react';
+import { Button, withStyles } from '@material-ui/core';
+import GetToken from '../GetToken';
+import authenticate from '../authenticate';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+import './about.css';
+
+const StyledButton = withStyles({
+  root: {
+    background: '#19869E',
+    borderRadius: 10,
+    border: 0,
+    color: 'white',
+    height: 48,
+    width: 144,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(250, 250, 250, 250)',
+    right: '575px'
+  },
+  label: {
+    textTransform: 'capitalize'
+  }
+})(Button);
+
+export default function Challenges() {
+  console.log('Challenge function called');
+  const [valid_playlists, setValidPlaylists] = useState({});
+
+  var token =
+    'BQDO4WAEBgvnADE_gdzHL8BlF_lNGCjXwhiEvfD139m7cNnuGS37x6eEA6oQ9bPzR3XSYzlsIKufFNhk-NkgCzne5mb8v2bqgDOmDrCZwviqU5WA2HM73BMS67TvqX9_ssQdwPPkzYFCTIQ8fOkyOfIK-2Ub8EE';
+  console.log('Token in challenges page: ', token);
+
+  var spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(token);
+
+  useEffect(() => {
+    async function setValidPlaylistsFn() {
+      await spotifyApi.getUserPlaylists().then(function(data) {
+        console.log('User playlists', data['items']);
+        for (var i in data['items']) {
+          //   console.log(data['items'][i]);
+          var playlist_info = data['items'][i];
+          var id_to_name_state = valid_playlists;
+          id_to_name_state[playlist_info['id']] = playlist_info['name'];
+          setValidPlaylists(id_to_name_state);
+          console.log(playlist_info['id'], playlist_info['name']);
+        }
+      });
+    }
+
+    setValidPlaylistsFn();
+    console.log('setValidPlaylistsFn', valid_playlists);
+  }, []);
+
+  return (
+    <div className="about">
+      <div className="about-header">
+        <div className="about-headerText">
+          <h2> Dummy Challenge </h2>
+          <h2> Dummy Challenge </h2>
+          <h2> Dummy Challenge </h2>
+          <h2> Dummy Challenge </h2>
+          <h2> Dummy Challenge </h2>
+          <hr />
+          {Object.keys(valid_playlists).map((key, index) => (
+            <p key={index}>
+              <button type="button">{valid_playlists[key]} </button>
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
