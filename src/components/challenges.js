704472bb145f3ffde492e5f23bc09962122b5b1d
ABiakolo <32600965+ABiakolo@users.noreply.github.com>
2020-04-01 13:20:40 -0700

    Buttons work individually, refresh doesnt, fixing bugs,

--- src/components/challenges.js
+++ src/components/challenges.js
@@ -4,73 +4,83 @@ import GetToken from '../GetToken';
 import authenticate from '../authenticate';
 import SpotifyWebApi from 'spotify-web-api-js';
 import createChallengeUtil from '../createChallengeUtil';
-
+import { getLocalToken } from '../token';
+ 
 import './about.css';
-
+ 
 const StyledButton = withStyles({
-  root: {
-    background: '#19869E',
-    borderRadius: 10,
-    border: 0,
-    color: 'white',
-    height: 48,
-    width: 144,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(250, 250, 250, 250)',
-    right: '575px'
-  },
-  label: {
-    textTransform: 'capitalize'
-  }
+ root: {
+   background: '#19869E',
+   borderRadius: 10,
+   border: 0,
+   color: 'white',
+   height: 48,
+   width: 144,
+   padding: '0 30px',
+   boxShadow: '0 3px 5px 2px rgba(250, 250, 250, 250)',
+   right: '575px'
+ },
+ label: {
+   textTransform: 'capitalize'
+ }
 })(Button);
-
-export default function Challenges() {
-  console.log('Challenge function called');
-  const [valid_playlists, setValidPlaylists] = useState({});
-
-  var token =
-    'BQC1uDvPB0id2CbTJ8BMXhgMqO0gYZ9LBVaniUZ9wjVRlLJhdQmIvQUZ6QoNRpuUJ2pjIdJ3IoEGg8hhntXepxDf1xUjnNavwY7YgldkpUhHxn_oPavaKfqRWnYh_O-Vnqc2XA1loiHw-HNZX66t5PYHL0VOFT0';
-
-  console.log('Token in challenges page: ', token);
-
-  var spotifyApi = new SpotifyWebApi();
-  spotifyApi.setAccessToken(token);
-
-  useEffect(() => {
-    async function setValidPlaylistsFn() {
-      await spotifyApi.getUserPlaylists().then(function(data) {
-        console.log('User playlists', data['items']);
-        for (var i in data['items']) {
-          //   console.log(data['items'][i]);
-          var playlist_info = data['items'][i];
-          var id_to_name_state = valid_playlists;
-          id_to_name_state[playlist_info['id']] = playlist_info['name'];
-          setValidPlaylists(id_to_name_state);
-          console.log(playlist_info['id'], playlist_info['name']);
-        }
-      });
+ 
+function Challenges() {
+ 
+ console.log('Challenge function called');
+ const [valid_playlists, setValidPlaylists] = useState({});
+ 
+ var token = getLocalToken();
+ 
+ var spotifyApi = new SpotifyWebApi();
+ spotifyApi.setAccessToken(token);
+ 
+ // const [click, setClick] = useState(() => {
+ //   const initialState = someExpensiveComputation(props);
+ //   return initialState;
+ // });
+ 
+ useEffect(() => {
+  async function setValidPlaylistsFn() {
+  let updatedValidPlaylists = [];
+  await spotifyApi.getUserPlaylists().then(function(data) {
+    console.log('User playlists', data['items']);
+    console.log("spotifyApi.getUserPlaylists");
+    for (var i in data['items']) {
+    // console.log(data['items'][i]);
+      var playlist_info = data['items'][i];
+      updatedValidPlaylists[playlist_info['id']] = playlist_info['name'];
+      console.log(playlist_info['id'], playlist_info['name']);
     }
+    // state does not actually update until the end of useEffect, you can't update
+    setValidPlaylists(updatedValidPlaylists);
+  });
+  }
+  
+  setValidPlaylistsFn();
+  }, []);
+ 
+ function handleclick(key)
+ {
+   return createChallengeUtil(key);
+ }
+ 
+ return (
+   <div className="about">
+     <div className="about-header">
+       <div className="about-headerText">
+         <h2> Challenges you can add</h2>
+         <hr />
+       {Object.keys(valid_playlists).map(key => (
+           <Button onClick={() => handleclick(key)}>
+             {valid_playlists[key]}
+           </Button>
 
-    setValidPlaylistsFn();
-  }, [valid_playlists, spotifyApi]);
-
-  return (
-    <div className="about">
-      <div className="about-header">
-        <div className="about-headerText">
-          <h2> Dummy Challenge </h2>
-          <h2> Dummy Challenge </h2>
-          <h2> Dummy Challenge </h2>
-          <h2> Dummy Challenge </h2>
-          <h2> Dummy Challenge </h2>
-          <hr />
-          {Object.keys(valid_playlists).map(key => (
-            <button type="button" onClick={createChallengeUtil(key)}>
-              {valid_playlists[key]}
-            </button>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+         ))}
+        
+       </div>
+     </div>
+   </div>
+ );
 }
+export default Challenges;
--- src/createChallengeUtil.js
+++ src/createChallengeUtil.js
@@ -1,19 +1,12 @@
 import SpotifyWebApi from 'spotify-web-api-js';
-
+import { getLocalToken } from './token';
 export default function createChallengeUtil(playlist_id) {
-  // Check if there are at least 20 songs
-  var token =
-    'BQC1uDvPB0id2CbTJ8BMXhgMqO0gYZ9LBVaniUZ9wjVRlLJhdQmIvQUZ6QoNRpuUJ2pjIdJ3IoEGg8hhntXepxDf1xUjnNavwY7YgldkpUhHxn_oPavaKfqRWnYh_O-Vnqc2XA1loiHw-HNZX66t5PYHL0VOFT0';
-
-  console.log('Token in challenges page: ', token);
+  var token = getLocalToken();
   var spotifyApi = new SpotifyWebApi();
   spotifyApi.setAccessToken(token);
 
   spotifyApi.getPlaylistTracks(playlist_id).then(function(value) {
     var playlist_ref = value['items'];
-    console.log(playlist_ref);
-    console.log('Playlist id/ Challenge id: ', playlist_id);
-    console.log('Playlist length: ', playlist_ref.length);
     var all_tracks = [];
     for (var i = 0; i < playlist_ref.length; i++) {
       var track_name_and_id =
@@ -21,8 +14,6 @@ export default function createChallengeUtil(playlist_id) {
       all_tracks.push(track_name_and_id);
     }
     console.log(all_tracks);
+    return all_tracks;
   });
-  alert('Check console');
-
-  // Populate Firebase with challenge data
 }
