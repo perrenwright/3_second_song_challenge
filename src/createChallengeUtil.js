9896dfe1990ae233da0841e5a56d967fe0393243
tonioshikanlu <toni.oshikanlu9@gmail.com>
2020-04-02 00:13:09 -0400

    Just made cleaner and more readable
    Feel free to remove if problematic. I deleted mithing.

--- src/createChallengeUtil.js
+++ src/createChallengeUtil.js
@@ -5,15 +5,26 @@ export default function createChallengeUtil(playlist_id) {
   var spotifyApi = new SpotifyWebApi();
   spotifyApi.setAccessToken(token);
 
-  spotifyApi.getPlaylistTracks(playlist_id).then(function(value) {
-    var playlist_ref = value['items'];
-    var all_tracks = [];
-    for (var i = 0; i < playlist_ref.length; i++) {
-      var track_name_and_id =
-        playlist_ref[i].track.name + ' : ' + playlist_ref[i].track.id;
-      all_tracks.push(track_name_and_id);
+  spotifyApi.getPlaylistTracks(playlist_id).then(function(data) {
+    // var playlist_ref = value['items'];
+    // var all_tracks = [];
+    // for (var i = 0; i < playlist_ref.length; i++) {
+    //   var track_name_and_id =
+    //     playlist_ref[i].track.name + ' : ' + playlist_ref[i].track.id;
+    //   all_tracks.push(track_name_and_id);
+    // }
+    // console.log(all_tracks);
+    // return all_tracks;
+    var plylist_images = []
+    const list = []
+    for (var i in data["items"]) {
+      var track_data = data['items'][i]['track']
+      var track_id = track_data["id"]
+      var track_name = track_data["name"]
+
+      list.push({playlist_id:playlist_id,track_id: track_id, name:track_name })
     }
-    console.log(all_tracks);
-    return all_tracks;
+    console.log(list)
+    return list
   });
 }
