0ff77321f80c0df75f9adf971a5fae4036f70976
prabinspkt <prabin4998@gmail.com>
2020-04-04 03:55:56 -0700

    Shuffle Question's choices and add right_choice to Track's JSON object.

--- src/getChallengeUtil.js
+++ src/getChallengeUtil.js
@@ -16,6 +16,18 @@ function getChallenge(challenge_id) {
   });
 }
 
+// Shuffle elements inside an array
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
 function getTrackJSON(unique_track_id) {
   return new Promise(function(resolve, reject) {
     let track_json = {};
@@ -25,12 +37,22 @@ function getTrackJSON(unique_track_id) {
       .get()
       .then(function(doc) {
         let track_data = doc.data();
+        // Temporarity store right choice and shuffle the choices
+        let right_choice = track_data.choice_A;
+        let shuffled_choices = shuffle([
+          track_data.choice_A,
+          track_data.choice_B,
+          track_data.choice_C,
+          track_data.choice_D
+        ]);
+
         track_json['track_id'] = track_data.track_id;
         track_json['start_time'] = track_data.start_time;
-        track_json['choice_A'] = track_data.choice_A;
-        track_json['choice_B'] = track_data.choice_B;
-        track_json['choice_C'] = track_data.choice_C;
-        track_json['choice_D'] = track_data.choice_D;
+        track_json['right_choice'] = right_choice;
+        track_json['choice_A'] = shuffled_choices[0];
+        track_json['choice_B'] = shuffled_choices[1];
+        track_json['choice_C'] = shuffled_choices[2];
+        track_json['choice_D'] = shuffled_choices[3];
         resolve(track_json);
       })
       .catch(function(error) {
