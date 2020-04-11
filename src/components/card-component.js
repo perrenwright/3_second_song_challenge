e47862527389499ea5d9ebdfa4c18321f46d835c
Mikayla Orange <mikayla.orange@bison.howard.edu>
2020-04-11 16:22:20 -0400

    Updated the component per Prabin's changes. There needs to be a way for the challenge to load upon clicking, but this is what shows so far Current Screenshot: https://screenshot.googleplex.com/xFcGvQNRZkQ

--- src/components/card-component.js
+++ src/components/card-component.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
-
+import './card-component.css';
 function CardComponent(props) {
 
     return ( 
-        <div>
-            <Button><img src={props.image} alt={props.name} /></Button>
-            <h3>{props.name}</h3>
-            <h4>Created by {props.creator}</h4>
+        <div className='card-component'>
+            <Button>
+                <img 
+                    className="playlist-image"
+                    src={props.image} 
+                    alt={props.name} />
+            </Button>
+            <h3 className='playlist-title'>{props.name}</h3>
+            <h4 className='playlist-creator-text'>Created by {props.creator}</h4>
         </div>
 
     )
--- src/components/challenges.js
+++ src/components/challenges.js
@@ -6,67 +6,50 @@ import firestoreRef from '../firebase';
 export default function Challenges()
 {
 
-    
-    //const db = firebase.firestore();
-    let name = "name";
-    let image = "";
-    let creator = "";
     console.log("calling function..")
     const [isFirebaseDone,setFirebaseDone] = useState(false);
+    const [name,setName] = useState('noName');
+    const [playlist,setPlaylsit] = useState([])
+    const [image,setImage] = useState('noImage');
+    const [creator,setCreator] = useState('noCreator');
 
     useEffect(() => {
+        /**
+         * Function that pulls data from firebase
+         * There may be a way to clean this up more when we get
+         * more playlists to pull.
+         */
         async function getPlaylists() {
-            await firestoreRef
-            .collection('Playlists')
-            .get()
-            .then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                    name = doc.data().playlist_info[0];
-                    console.log("Name is: ", name)
-                    image = doc.data().playlist_info[1];
-                    creator = doc.data().playlist_info[2];
-                    setFirebaseDone(true);
-                });
+           let querySnapshot =  await firestoreRef
+            .collection('challenge_test')
+            .get();
+            querySnapshot.forEach(function(doc) {
+                playlist.push([doc.data().challenge_name,doc.data().challenge_image,doc.data().challenge_creator,doc.id]);
+                console.log(playlist)
             });
+
             console.log('Getting playlists finished');
+            setFirebaseDone(true);
         }
         getPlaylists();
-    });
-    //let docRef = db.collection("Playlists").doc("playlist_1");
-//     async function getPlaylists() {
-//     await firestoreRef.collection('Playlists')
-//     .get()
-//     .then(function(querySnapshot) {
-//         if (doc.exists) {
-//             console.log("Document data:", doc.data());
-//             let playlist_info = doc.get('playlist_info');
-//             console.log(playlist_info);
-//             name = playlist_info[0];
-//             image = playlist_info[1];
-//             creator = playlist_info[2]
-//             setFirebaseDone(true);
 
-
-//         } else {
-//             // doc.data() will be undefined in this case
-//             console.log("No such document!");
-//         }
-//     }).catch(function(error) {
-//         console.log("Error getting document:", error);
-//     });
-//         console.log("Getting playlist Finished.")
-
-// }
-    console.log(name)
-    console.log("we here!");
-
+    },[]);
     return(
         <div>
             <header>
                 {console.log("rendering component...")}
-                {isFirebaseDone && <CardComponent name={name}
-                               image={image}
-                               creator={creator}/>}
+                {playlist.map((row)=> (
+
+                        <CardComponent key={row[0]}
+                                       name={row[0]}
+                                       image={row[1]}
+                                       creator={row[2]}
+                        />
+
+                )
+                )
+}
+
             </header>
         </div>
         
