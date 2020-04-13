ba573025ca8d6e9d8e9cce6d8ec0b4f7103c9594
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2020-04-13 15:37:39 -0400

    I made global leaderboard grab all the current high scorers (#21)
    * I made global leaderboard grab all the current high scorers
    
    * CI env update
    
    * Trying to remove warnings.
    
    * More warnings cleanup
    
    Co-authored-by: prabinspkt <prabin4998@gmail.com>

--- src/GetToken.js
+++ src/GetToken.js
@@ -1,22 +1,19 @@
-import SpotifyWebApi from 'spotify-web-api-js';
-import {setLocalToken} from './token';
+import { setLocalToken } from './token';
 
 export default function getToken() {
+  // This function grabs the user's access token from the url so we can grab the information
+  // about that user from the database.
+  var hashParams = {};
+  var e,
+    r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+  e = r.exec(q);
+  while (e) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+    e = r.exec(q);
+  }
 
-    // This function grabs the user's access token from the url so we can grab the information 
-    // about that user from the database.
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
-    }
-
-    const token = hashParams.access_token;
-    setLocalToken(token);
-    return token;
-  
+  const token = hashParams.access_token;
+  setLocalToken(token);
+  return token;
 }
-
--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import firebase from 'firebase';
 import authenticate from '../authenticate.js';
- 
+
 export default function LoginButton() {
   return (
     <div className="Toggle">
       <button className="myButton" onClick={() => authenticate()}>
         Login
       </button>
-    </div>);
+    </div>
+  );
 }
-
