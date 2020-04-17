ced347c57a415b2a08a80f13872ba3566bfcbd12
tonioshikanlu <toni.oshikanlu9@gmail.com>
2020-04-17 19:31:29 -0400

    state persist

--- src/GetToken.js
+++ src/GetToken.js
@@ -1,23 +1,9 @@
 import { setLocalToken, getLocalToken } from './token';
-import React from 'react'
 
-const useStateWithLocalStorage = localStorageKey => {
-  const [value, setValue] = React.useState(
-    localStorage.getItem(localStorageKey) || ''
-  );
-  React.useEffect(() => {
-    localStorage.setItem(localStorageKey, value);
-    // eslint-disable-next-line
-  }, []);
-  return [value, setValue];
-};
 
-export default function GetToken() {
+export default function getToken() {
   // This function grabs the user's access token from the url so we can grab the information
   // about that user from the database.
-  const [value, setValue] = useStateWithLocalStorage(
-    'myValueInLocalStorage'
-  );
   var hashParams = {};
   var e,
     r = /([^&;=]+)=?([^&;]*)/g,
@@ -28,58 +14,17 @@ export default function GetToken() {
     e = r.exec(q);
   }
 
-  const token = hashParams.access_token;
-  React.useEffect(() => {
+  var token = hashParams.access_token;
+  if (token)
+  {
+    localStorage.setItem('localStorageKey', token);
+  }else{token = localStorage.getItem('localStorageKey');}
 
-    setValue(token)
-    setLocalToken(value);
-    console.log('Token: ', value);
-    var local_token = getLocalToken();
-    console.log('Local Storage token', local_token);
-    // eslint-disable-next-line
-  }, [token]);
 
+  setLocalToken(token);
+  console.log('Token: ', token);
+  var local_token = getLocalToken();
+  console.log('Local Storage token', local_token);
   return token;
+
 }
-
-
-// const useStateWithLocalStorage = localStorageKey => {
-//   const [value, setValue] = React.useState(
-//     localStorage.getItem(localStorageKey) || ''
-//   );
-//   React.useEffect(() => {
-//     localStorage.setItem(localStorageKey, value);
-//     // eslint-disable-next-line
-//   }, []);
-//   return [value, setValue];
-// };
-
-// export default function GetToken() {
-// const [value, setValue] = useStateWithLocalStorage(
-//   'myValueInLocalStorage'
-// );
-//   // This function grabs the user's access token from the url so we can grab the information
-//   // about that user from the database.
-//   var hashParams = {};
-//   var e,
-//     r = /([^&;=]+)=?([^&;]*)/g,
-//     q = window.location.hash.substring(1);
-//   e = r.exec(q);
-//   while (e) {
-//     hashParams[e[1]] = decodeURIComponent(e[2]);
-//     e = r.exec(q);
-//   }
-//   const token = hashParams.access_token;
-//   React.useEffect(() => {
-
-//     setValue(token)
-//     setLocalToken(value);
-//     console.log('Token: ', value);
-//     var local_token = getLocalToken();
-//     console.log('Local Storage token', local_token);
-//     // eslint-disable-next-line
-//   }, [token]);
-
-//   return token;
-
-
