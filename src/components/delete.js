b40ae9d9b67b4e5446e3c84bab6aafea16295666
ABiakolo <32600965+ABiakolo@users.noreply.github.com>
2020-04-19 14:19:08 -0700

    Update delete.js

--- src/components/delete.js
+++ src/components/delete.js
@@ -1,15 +1,12 @@
 import React,{useState, useEffect} from 'react';
 // import PlayChallenge from './ChallengePage';
 import {Button} from '@material-ui/core';
-import CardComponent from './card-component';
 import './firebase.js';
-import firebase, { database } from 'firebase';
+import firebase from 'firebase';
 // import firebase, {database} from './firebase.js'
 import firestoreRef from '../firebase';
 import { getLocalToken } from '../token';
 import SpotifyWebApi from 'spotify-web-api-js';
-import {GAME_STATE} from '../gamestate_enum.js';
-import { useRef } from "react";
 import './about.css';
 import './challenges.css';
 import './delete.css'
@@ -31,6 +28,11 @@ function Delete()
         }
 
     useEffect(() => {
+        /**
+         * Function that pulls data from firebase
+         * There may be a way to clean this up more when we get
+         * more playlists to pull.
+         */
         async function getPlaylists() {
           var username = await getUsername()
           let temp_playlist = [];
@@ -38,6 +40,7 @@ function Delete()
             .collection('challenge_test')
             .get();
             querySnapshot.forEach(function(doc) {
+
                 if (doc.data().challenge_creator === username){
                     temp_playlist.push([doc.data().challenge_name,doc.data().challenge_image,doc.data().challenge_creator,doc.id]);
                     console.log(playlist)
@@ -47,24 +50,9 @@ function Delete()
             console.log('Getting playlists finished');
         }
         getPlaylists();
+
     },[]);
 
-    // function rerender()
-    // {
-    //     return window.location.reload();
-    // }
-        function deleteButton(playlist_key)
-        {
-            Object.keys(playlist).forEach(function(key){
-                if (key === playlist_key)
-                {
-                    deleteChallenge(playlist[playlist_key][0], playlist[playlist_key][2])
-                    delete playlist[playlist_key]
-                }
-            });
-            // return rerender();
-        }
-    
     async function deleteChallenge(playlist_name, username) {
         console.log(playlist_name)
         let db = firebase.firestore();
@@ -92,30 +80,28 @@ function Delete()
         .catch(function(error) {
         console.log("Error getting documents: ", error);
         });
-        alert('Document Deleted.') 
+        return alert('Document Deleted.')
     }
 
     return(
     //  <div className="about">
        <div className="class1">
-        <div className="about-titleText">
-            <h2>Delete Your Challenges</h2>
+        <div className="about-headerText">
+            <h2> Challenges you can delete</h2>
             <hr/>
             <header>
                 {console.log("rendering component...")}
                 {Object.keys(playlist).map((key) => (
-                    <Button onClick={()=> deleteButton(key)}>
+                    <Button onClick={()=> deleteChallenge(playlist[key][0], playlist[key][2])}>
                     <img className="photo" src={playlist[key][1]} alt='img' />
                     {playlist[key][0]}
+
                     </Button>
-                    
           ))}
-          
             </header>
         </div>
         </div>
-        
+
     );
 }
 export default Delete;
-
