44b392815147cb75bc68f0bfcf19543da5462320
ABiakolo <32600965+ABiakolo@users.noreply.github.com>
2020-04-20 16:17:37 -0700

    Added Prabin's changes to addChallenge

--- src/components/addChallenge.js
+++ src/components/addChallenge.js
@@ -6,7 +6,7 @@ import createChallengeUtil from '../createChallengeUtil';
 import { getLocalToken } from '../token';
 import './addChallenge.css';
 import './challenges.css';
-
+import firestoreRef from '../firebase';
 
 
 function Challenges() {
@@ -25,6 +25,8 @@ function Challenges() {
 
   console.log('Challenge function called');
   const [valid_playlists, setValidPlaylists] = useState({});
+  const [unused_playlists, setUnusedPlaylists] = useState([]);
+  const [used_playlists, setUsedPlaylists] = useState([]);
   const [open, setOpen] = useState(false);
   const [message,setMessage] = useState('');
   var token = getLocalToken();
@@ -34,22 +36,54 @@ function Challenges() {
 
   useEffect(() => {
     async function setValidPlaylistsFn() {
-      let updatedValidPlaylists = [];
-      await spotifyApi.getUserPlaylists().then(function (data) {
-        console.log('User playlists', data['items']);
-        console.log('spotifyApi.getUserPlaylists');
-        for (var i in data['items']) {
-          // console.log(data['items'][i]);
-          var playlist_info = data['items'][i];
-          updatedValidPlaylists[playlist_info['id']] = [playlist_info['name'],playlist_info['images'][0].url];
-          console.log(playlist_info['id'], playlist_info['name'],playlist_info['image']);
-        }
-        // state does not actually update until the end of useEffect, you can't update
-        setValidPlaylists(updatedValidPlaylists);
+  //     let updatedValidPlaylists = [];
+  //     await spotifyApi.getUserPlaylists().then(function (data) {
+  //       console.log('User playlists', data['items']);
+  //       console.log('spotifyApi.getUserPlaylists');
+  //       for (var i in data['items']) {
+  //         // console.log(data['items'][i]);
+  //         var playlist_info = data['items'][i];
+  //         updatedValidPlaylists[playlist_info['id']] = [playlist_info['name'],playlist_info['images'][0].url];
+  //         console.log(playlist_info['id'], playlist_info['name'],playlist_info['image']);
+  //       }
+  //       // state does not actually update until the end of useEffect, you can't update
+  //       setValidPlaylists(updatedValidPlaylists);
+  //     });
+  //   }
+  //   setValidPlaylistsFn();
+  // },[] );
+
+  let firestore_playlists = [];
+  await firestoreRef
+    .collection('challenge_test')
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        firestore_playlists.push(doc.id);
       });
-    }
-    setValidPlaylistsFn();
-  },[] );
+    });
+    let updatedValidPlaylists = [];
+    let temp_unused_playlists = [];
+    let temp_used_playlists = [];
+    await spotifyApi.getUserPlaylists().then(function (data) {
+      for (var i in data['items']) {
+        var playlist_info = data['items'][i];
+        updatedValidPlaylists[playlist_info['id']] = [playlist_info['name'],playlist_info['images'][0].url];
+        if (firestore_playlists.includes(playlist_info['id'])) {
+          temp_used_playlists.push(playlist_info['id']);
+        } else {
+          temp_unused_playlists.push(playlist_info['id']);
+        }
+      console.log(playlist_info['id'], playlist_info['name']);
+      }
+      setValidPlaylists(updatedValidPlaylists);
+      setUsedPlaylists(temp_used_playlists);
+      setUnusedPlaylists(temp_unused_playlists);
+    });
+  }
+  setValidPlaylistsFn();
+  // eslint-disable-next-line
+}, []);
 
   function handleClick(key) {
     console.log("Calledin create Challenge Util")
@@ -75,7 +109,8 @@ function Challenges() {
     <div className="about-titleText2">
           <h2> Add Your Challenges</h2>
           <hr />
-          {Object.keys(valid_playlists).map((key) => (
+          {unused_playlists.map((key) => {
+          return (
             <div style={{display: 'inline-block'}} className="content">
             <Button onClick={() => handleClick(key)}>
             <img className="addChallenge-image" src={valid_playlists[key][1]} alt={valid_playlists[key][0]}/>
@@ -87,12 +122,24 @@ function Challenges() {
         </Alert>
       </Snackbar>
             </div>
+      );
+      })}
+       <h2>Challenges you have created</h2>
+       <hr />
+        {used_playlists.map((key) => {
+          return (
+            <div style={{display: 'inline-block'}} className="content">
+            <Button>
+            <img className="addChallenge-image" src={valid_playlists[key][1]} alt={valid_playlists[key][0]}/>
+            </Button>
 
-          ))}
-          
-        </div>
+            <h6>{valid_playlists[key][0]}</h6>
+            </div>
+          );
+        })}
       </div>
-    // </div>
+    </div>
   );
 }
+  
 export default Challenges;
