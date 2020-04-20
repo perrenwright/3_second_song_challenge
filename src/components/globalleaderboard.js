987902c0c8e5b35c6f474193b60cbb0aeb440a46
prabinspkt <prabin4998@gmail.com>
2020-04-20 14:08:07 -0700

    Managed global irrational data read from firebase. Displays correct 5 top highest scores

--- src/components/globalleaderboard.js
+++ src/components/globalleaderboard.js
@@ -4,19 +4,16 @@ import './globalleaderboard.css';
 
 export default function Leaderboard() {
   console.log('Leaderboard function called');
-  const [highest_scores, setHighestScores] = useState([]);
-  const [score_to_user, setScoreToUser] = useState({});
+  const [scores_info, setScoresInfo] = useState([]);
 
   useEffect(() => {
     async function getHighestScores() {
+      let temp_scores_info = [];
       await firestoreRef
         .collection('challenge_test')
         .get()
         .then(function (querySnapshot) {
-          var temp_scores_array = [...highest_scores];
-          var temp_score_to_user = {...score_to_user};
           querySnapshot.forEach(function (doc) {
-
             if (doc.data().highest_score > 0) {
               var temp_score = doc.data().highest_score;
               var temp_scorer = doc.data().highest_scorer;
@@ -25,20 +22,20 @@ export default function Leaderboard() {
               console.log('Temp Score is:', temp_score);
               console.log('challenge_name is: ', challenge_name);
               console.log('User is: ', temp_scorer);
-              temp_scores_array.push([temp_score, temp_scorer]);
-              temp_score_to_user[challenge_name] = [temp_score, temp_scorer];
-              temp_scores_array.sort(function (a, b) {
-                return b[0] - a[0];
-              });
-              setHighestScores(temp_scores_array);
-              setScoreToUser(temp_score_to_user);
+              temp_scores_info.push([challenge_name, temp_score, temp_scorer]);
             }
           });
         });
+      temp_scores_info.sort(function (a, b) {
+        return b[1] - a[1];
+      });
+      temp_scores_info.slice(0, 5);
+      console.log(temp_scores_info);
+      setScoresInfo(temp_scores_info);
       console.log('getHighestScores call finished.');
     }
     getHighestScores();
-  // eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
   return (
@@ -46,13 +43,14 @@ export default function Leaderboard() {
       <font color="black">
         <h2>Global Leaderboard</h2>
         <hr />
-        {Object.keys(highest_scores).map((key) => (
-          <div>
-            <div>
-              {highest_scores[key][1]}: {highest_scores[key][0]}
-            </div>
-          </div>
-        ))}
+        {console.log('Scores value test in render', scores_info)}
+        {scores_info.map((score_info) => {
+          return (
+            <p>
+              {score_info[0]} : {score_info[1]} : {score_info[2]}{' '}
+            </p>
+          );
+        })}
       </font>
     </div>
   );
