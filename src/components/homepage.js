d730bfb60fae4e414c5f55b978771ec8615cf738
Perren Wright <34662374+perrenwright@users.noreply.github.com>
2020-04-24 17:01:52 -0500

    fixed errors

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,12 +1,4 @@
 import React from 'react';
-import MenuAppBar from './appNav';
-import globalleaderboard from './globalleaderboard';
-import challenges from './challenges';
-import about from './about';
-import Delete from './delete';
-import Contact from './contact';
-import addchallenge from './addChallenge';
-import { Route, Switch } from 'react-router-dom';
 
 export default function Homepage(){
     return (
