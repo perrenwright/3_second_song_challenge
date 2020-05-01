e1108482085f41818534e3bf26f7c07682afea73
ABiakolo <32600965+ABiakolo@users.noreply.github.com>
2020-05-01 14:36:58 -0700

    finished making delete.js

--- src/tests/delete.test.js
+++ src/tests/delete.test.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import Delete from '../components/delete';
-import addChallenge from '../components/addChallenge';
 import { render, fireEvent, act } from '@testing-library/react';
 import fs from 'fs'; // File system import
 import firebase from 'firebase';
 import * as firebaseTesting from '@firebase/testing'
 
 
-// const rules = fs.readFileSync("firestore.rules", "utf8");
 
-test('Test Delete', async () => {
+// when you run the tests for this to work type in " firebase emulators:exec --only firestore 'npm test' "
 
-    // add mock data and wait till it's finished adding
-    await db.collection("challenge_test").doc("223").set({
-      challenge_name: "challenge_name"
+const projectId = "secondsongchallenge";
+const rules = fs.readFileSync("firestore.rules", "utf8");
+
+beforeEach(async () => {
+  // Clear the database between tests
+  await firebaseTesting.clearFirestoreData({ projectId });
+  await firebaseTesting.loadFirestoreRules({ projectId, rules });
+});
+
+describe("Delete", () => {
+
+    const firebaseApp = firebase.initializeApp({
+      projectId: projectId
     });
 
-    // render(<addChallenge />);
+    const db = firebaseApp.firestore();
+        db.settings({
+        host: "localhost:8080",
+        ssl: false
+    });
 
-    // The default values we inserted should be rendered
-    firebase.assertSucceeds(app.firestore().collection("challenge-test").doc("223").get());
-    render(<Delete />);
-    firebase.assertFails(app.firestore().collection("challenge-test").doc("223").get());
+    test('Test Delete', async () => {
+
+        // add mock data and wait till it's finished adding
+        await db.collection("challenge_test").doc("223").set({
+          challenge_name: "challenge_name"
+        });
+
+        // render(<addChallenge />);
+
+        // The default values we inserted should be rendered
+        firebaseTesting.assertSucceeds(db.collection("challenge-test").doc("223").get());
+        render(<Delete />);
+        firebaseTesting.assertFails(db.collection("challenge-test").doc("223").get());
   });
+
+}); 
